20c5774ebcf9596da2f299091d559808a071242b
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-24 19:25:43 -0400

    Agrega Parcelable en data class usuario

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/model/Usuario.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/model/Usuario.kt
@@ -1,9 +1,42 @@
 package cl.jpinodev.walletarqlifecycle.feature_usuario.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Usuario(
     val user_id: String ,
     val nombre: String ,
     val email: String ,
     val contrasena: String,
     val fecha_creacion: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(user_id)
+        parcel.writeString(nombre)
+        parcel.writeString(email)
+        parcel.writeString(contrasena)
+        parcel.writeString(fecha_creacion)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Usuario> {
+        override fun createFromParcel(parcel: Parcel): Usuario {
+            return Usuario(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Usuario?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
