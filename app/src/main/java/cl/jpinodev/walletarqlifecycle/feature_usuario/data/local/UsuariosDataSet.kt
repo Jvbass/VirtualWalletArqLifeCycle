6b83b19cf48ea85f8c90101a88a07645cf77477d
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-24 19:24:33 -0400

    Agrega funcion getAllUsuarios() en UsuariosDataSet

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/local/UsuariosDataSet.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/local/UsuariosDataSet.kt
@@ -5,12 +5,15 @@ import cl.jpinodev.walletarqlifecycle.feature_usuario.data.model.Usuario
 class UsuariosDataSet {
     fun createUsuarios(): MutableList<Usuario> {
         return mutableListOf(
-            Usuario("User01", "Juan", "123123", "123123", "14-10-2021"),
+            Usuario("User01", "Juan", "123", "123", "14-10-2021"),
             Usuario("User02", "Pedro", "pedro@admin", "123123", "14-10-2021"),
             Usuario("User03", "Maria", "maria@admin", "123123", "14-10-2021"),
             Usuario("User04", "Jose", "jose@admin", "123123", "14-10-2021"),
             Usuario("User05", "Luis", "luis@admin", "123123", "14-10-2021"),
             Usuario("User06", "Ana", "ana@admin", "123123", "14-10-2021"),
         )
+    }
+    fun getAllUsuarios(): MutableList<Usuario>? {
+        return createUsuarios()
     }
 }
