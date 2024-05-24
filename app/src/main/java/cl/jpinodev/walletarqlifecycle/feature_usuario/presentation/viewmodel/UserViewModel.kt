66cba43caa444973f8650295f6806fa3304a400d
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-24 19:26:36 -0400

    Modificacion de init seteando usuarios para acceder a usuarios

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/viewmodel/UserViewModel.kt
@@ -4,29 +4,36 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cl.jpinodev.walletarqlifecycle.feature_usuario.data.local.UsuariosDataSet
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.model.Usuario
 
-class UserViewModel: ViewModel() {
+class UserViewModel : ViewModel() {
     private val _usuarios = MutableLiveData<MutableList<Usuario>>()
     val usuarios: LiveData<MutableList<Usuario>> get() = _usuarios
+    private val _usuarioConectado = MutableLiveData<Usuario>()
+    val usuarioConectado: LiveData<Usuario> get() = _usuarioConectado
 
     /*
     fun setUsuario(usuario: Usuario) {
         _usuarios.value = usuario
     }
     */
-  init {
-      _usuarios.value = mutableListOf(Usuario("User01", "Juan", "123",
-          "123",  "14-10-2021"))
-  }
-
-    private val _usuarioConectado = MutableLiveData<Usuario>()
-    val usuarioConectado: LiveData<Usuario> get() = _usuarioConectado
+    init {
+        _usuarios.value =  UsuariosDataSet().getAllUsuarios()
+        //_usuarioConectado.value = _usuarios.value?.first()
+    }
 
     fun setUsuarioConectado(usuario: Usuario) { //Setea el usuario conectado
         _usuarioConectado.value = usuario
     }
+
     fun autenticarUsuario(email: String, password: String): Usuario? {
-        return _usuarios.value?.find { it.email == email && it.contrasena == password } //devuelve el usuario donde email y contrasena coinsidan
+        val usuario = _usuarios.value?.find { it.email == email && it.contrasena == password }
+        if (usuario != null) {
+            setUsuarioConectado(usuario)
+            return usuario
+        } else {
+            return null
+        }
     }
- }
+}
