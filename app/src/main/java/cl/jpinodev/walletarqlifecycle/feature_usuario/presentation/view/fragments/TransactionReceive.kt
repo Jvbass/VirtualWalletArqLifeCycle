61c05e5a02280711ebbe6d0940845a87eb876555
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-30 19:20:55 -0400

    Implementa Spinner seleccion de usuario en Transaction Receive

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/fragments/TransactionReceive.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/fragments/TransactionReceive.kt
@@ -1,17 +1,20 @@
 package cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
-import cl.jpinodev.walletarqlifecycle.R
 import cl.jpinodev.walletarqlifecycle.databinding.FragmentTransactionReceiveBinding
-import cl.jpinodev.walletarqlifecycle.databinding.FragmentTransactionSendBinding
+import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.adapter.UsersAdapter
+import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.viewmodel.WalletViewModel
 
 class TransactionReceive : Fragment() {
     private lateinit var binding: FragmentTransactionReceiveBinding
+    private val viewModel: WalletViewModel by activityViewModels()
+    private lateinit var adapter: UsersAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +30,11 @@ class TransactionReceive : Fragment() {
 
         binding.materialToolbar.setNavigationOnClickListener {
             navController.navigateUp()
+        }
+
+        viewModel.usuarios.observe(viewLifecycleOwner) { usuarios ->
+            adapter = UsersAdapter(requireContext(), usuarios)
+            binding.spinnerReceiveMoney.adapter = adapter
         }
 
     }
