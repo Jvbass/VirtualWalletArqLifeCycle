7eac322285637c9c47e7ecc1b8a1b2923b59173f
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-06-07 15:36:29 -0400

    Implementacion de funcionalidad para crear usuario

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/activity/SignUpActivity.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/activity/SignUpActivity.kt
@@ -1,14 +1,48 @@
 package cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.view.activity
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import cl.jpinodev.walletarqlifecycle.databinding.ActivitySignUpBinding
+import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.utils.ToastUtils
+import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.viewmodel.WalletViewModel
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private val walletViewModel: WalletViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.linkLogin.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+        // boton crear cuenta
+        binding.crearCuentaBtn.setOnClickListener {
+            val nombre = binding.nombreEditText.text.toString()
+            val apellido = binding.apellidoEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val contrasena = binding.passwordEditText.text.toString()
+            val contrasenaConfirm = binding.passwordRepeatEditText.text.toString()
+
+            //validando campos
+            if (nombre.isEmpty() || apellido.isEmpty() || email.isEmpty() || contrasena.isEmpty()) {
+                ToastUtils.showCustomToast(this, "Todos los campos son obligatorios")
+
+            } else if (contrasenaConfirm != contrasena) {
+                ToastUtils.showCustomToast(this, "contraseñas no coinciden")
+            } else {
+                val nuevoUsuario =
+                    walletViewModel.crearNuevoUsuario(nombre, apellido, email, contrasena)
+                ToastUtils.showCustomToast(this, "Usuario ${nuevoUsuario.nombre} creado con éxito")
+
+                val intent = Intent(this, LogInActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/viewmodel/WalletViewModel.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/viewmodel/WalletViewModel.kt
@@ -5,24 +5,27 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import cl.jpinodev.walletarqlifecycle.R
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.local.AccountDataSet
+import cl.jpinodev.walletarqlifecycle.feature_usuario.data.local.DataHolder
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.local.TransactionDataSet
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.local.UsuariosDataSet
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.model.Account
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.model.Transaction
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.model.Usuario
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 /*
  * ViewModel para manejar los datos y operaciones relacionadas con usuarios, transacciones y cuentas.
- *
-*/
+ */
 class WalletViewModel : ViewModel() {
 
     /*
     * Variables privadas para usuarios, transacciones y cuentas
     * */
-    private val _usuarios = MutableLiveData<MutableList<Usuario>>()
-    private val _transactions = MutableLiveData<MutableList<Transaction>>()
-    private val _accounts = MutableLiveData<MutableList<Account>>()
+    private val _usuarios = MutableLiveData<MutableList<Usuario>>().apply { value = DataHolder.usuarios }
+    private val _transactions = MutableLiveData<MutableList<Transaction>>().apply { value = DataHolder.transactions }
+    private val _accounts = MutableLiveData<MutableList<Account>>().apply { value = DataHolder.accounts }
     private val _usuarioConectado = MutableLiveData<Usuario>()
 
     /*
@@ -36,11 +39,11 @@ class WalletViewModel : ViewModel() {
     /*
     *  Inicializa los usuarios, las transacciones y las cuentas
     * */
-    init {
+    /*init {
         _usuarios.value = UsuariosDataSet().getAllUsuarios()
         _transactions.value = TransactionDataSet().getTransactionList()
         _accounts.value = AccountDataSet().getAllAccounts()
-    }
+    }*/
 
     /*
     * Metodo para definir el usuario conectado
@@ -101,6 +104,7 @@ class WalletViewModel : ViewModel() {
             dateTime = dateTime
         )
         _transactions.value?.add(newTransaction)
+        _transactions.value = DataHolder.transactions
         updateBalances(senderId, receiverId, amount)
     }
 
@@ -133,6 +137,8 @@ class WalletViewModel : ViewModel() {
             it.balance += amount // suma saldo al receptor
         }
         _accounts.value = accounts
+        DataHolder.accounts = accounts // Actualizar DataHolder
+
     }// end of updateBalances
 
     /*
@@ -145,6 +151,8 @@ class WalletViewModel : ViewModel() {
         receiverAccount?.let {
             it.balance += amount
             _accounts.value = _accounts.value // Notificar la actualización
+            DataHolder.accounts = _accounts.value!! // Actualizar DataHolder
+
         }
     }
 
@@ -156,7 +164,41 @@ class WalletViewModel : ViewModel() {
             idReceriver = userId,
             dateTime = dateTime
         )
-        _transactions.value?.add(newTransaction)
+        DataHolder.transactions.add(newTransaction)
+        _transactions.value = DataHolder.transactions  // Añadir a DataHolder
     }
 
+    /*
+    * Metodo para crear nuevo usuario
+    * */
+    fun crearNuevoUsuario(nombre: String, apellido: String, email: String, contrasena: String): Usuario {
+        val nuevoUsuario = Usuario(
+            user_id = generateUserId(),
+            nombre = nombre,
+            apellido = apellido,
+            email = email,
+            contrasena = contrasena,
+            fecha_creacion = obtenerFechaActual()
+        )
+        _usuarios.value?.add(nuevoUsuario)
+        DataHolder.usuarios = _usuarios.value!!
+
+        // Asignar una cuenta al nuevo usuario con un saldo inicial de 500
+        val nuevaCuenta = Account(
+            user_id = nuevoUsuario.user_id,
+            balance = 500.0
+        )
+        _accounts.value?.add(nuevaCuenta)
+        DataHolder.accounts = _accounts.value!!
+
+        return nuevoUsuario
+    }
+    private fun generateUserId(): String {
+        return "User${_usuarios.value?.size?.plus(1)}"
+    }
+
+    private fun obtenerFechaActual(): String {
+        val formatFecha = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        return formatFecha.format(Date())
+    }
 }// end of class WalletViewModel
