92d391da82256821f32c543360510611a26a1aab
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-06-07 02:20:30 -0400

    Implementa funcionalidad para enviar dinero a otra cuenta, metodo para registrar datetime actual y local

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/fragments/TransactionSend.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/fragments/TransactionSend.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
 import cl.jpinodev.walletarqlifecycle.databinding.FragmentTransactionSendBinding
+import cl.jpinodev.walletarqlifecycle.feature_usuario.data.model.Usuario
 import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.adapter.UsersAdapter
 import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.viewmodel.WalletViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class TransactionSend : Fragment() {
     private lateinit var binding: FragmentTransactionSendBinding
@@ -33,8 +38,41 @@ class TransactionSend : Fragment() {
             navController.navigateUp()
         }
         viewModel.usuarios.observe(viewLifecycleOwner) { usuarios ->
-            adapter = UsersAdapter(requireContext(), usuarios)
+            adapter = UsersAdapter(requireContext(), usuarios, viewModel)
             binding.spinnerSendMoney.adapter = adapter
         }
+        binding.sendButton.setOnClickListener {
+            val receiver = binding.spinnerSendMoney.selectedItem as Usuario
+            val amount = binding.amountEditText.text.toString().toDoubleOrNull()
+
+            if (amount != null && amount > 0) {
+                val senderId = viewModel.usuarioConectado.value?.user_id
+                if (senderId != null) {
+                    val senderBalance = viewModel.getBalanceForUser(senderId)
+                    if (senderBalance >= amount) {
+
+                        viewModel.addTransaction(
+                            senderId,
+                            receiver.user_id,
+                            amount,
+                            getCurrentDateTime()
+                        )
+                        Toast.makeText(context, "Dinero enviado con éxito", Toast.LENGTH_SHORT)
+                            .show()
+                        navController.navigateUp()
+                    }
+                    else {
+                        Toast.makeText(context, "Saldo insuficiente", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(context, "Ingrese un monto válido", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getCurrentDateTime(): String {
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return formatter.format(Date())
     }
 }
