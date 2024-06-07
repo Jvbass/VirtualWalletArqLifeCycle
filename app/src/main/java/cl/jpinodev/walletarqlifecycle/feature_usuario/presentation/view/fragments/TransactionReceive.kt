2362beac239a82e2281cb580edaf6585193d8b02
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-06-07 12:07:59 -0400

    Implementa funcionalidad para deposito de dinero usuario conectado

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/fragments/TransactionReceive.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/view/fragments/TransactionReceive.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
 import cl.jpinodev.walletarqlifecycle.databinding.FragmentTransactionReceiveBinding
 import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.adapter.UsersAdapter
+import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.utils.ToastUtils
 import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.viewmodel.WalletViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class TransactionReceive : Fragment() {
     private lateinit var binding: FragmentTransactionReceiveBinding
@@ -23,6 +28,7 @@ class TransactionReceive : Fragment() {
         binding = FragmentTransactionReceiveBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -32,11 +38,35 @@ class TransactionReceive : Fragment() {
             navController.navigateUp()
         }
 
-         viewModel.usuarioConectado.observe(viewLifecycleOwner) {
+        viewModel.usuarioConectado.observe(viewLifecycleOwner) {
             val imageResource = viewModel.getUserImageResource(it.user_id)
             binding.nombreUsuario.text = it.nombre
             binding.apellidoUsuario.text = it.apellido
             binding.imageUsuario.setImageResource(imageResource)
         }
+        binding.buttonReceive.setOnClickListener {
+            val amount = binding.txtAmountSend.text.toString().toDoubleOrNull()
+            val connectedUserId = viewModel.usuarioConectado.value?.user_id
+            if (amount != null && amount > 0) {
+                if (connectedUserId != null) {
+                    viewModel.receiveMoney(connectedUserId, amount)
+                    viewModel.addTransactionDeposit(
+                        connectedUserId,
+                        amount,
+                        getCurrentDateTime()
+                    )
+                }
+                ToastUtils.showCustomToast(requireContext(), "Deposito realizado con exito")
+                navController.navigateUp()
+//                binding.txtAmountSend.text?.clear()
+            } else {
+                ToastUtils.showCustomToast(requireContext(), "Ingrese un monto valido valido")
+            }
+        }
+    }
+
+    private fun getCurrentDateTime(): String {
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return formatter.format(Date())
     }
 }
