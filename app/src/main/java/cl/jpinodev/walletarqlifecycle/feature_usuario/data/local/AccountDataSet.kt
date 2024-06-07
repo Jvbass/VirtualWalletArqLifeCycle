b0f16d8f3367e75ad76ad9f90ccbf74f2de88800
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-06-07 02:15:48 -0400

    Actualiza lista de cuentas con usuarios existentes

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/local/AccountDataSet.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/local/AccountDataSet.kt
@@ -10,7 +10,9 @@ class AccountDataSet {
             Account("acc3", "User03", 300.0),
             Account("acc4", "User04", 400.0),
             Account("acc5", "User05", 500.0),
-            Account("acc6", "User06", 600.0)
+            Account("acc6", "User06", 600.0),
+            Account("acc7", "User07", 700.0),
+            Account("acc8", "User08", 800.0),
         )
     }
     fun getAllAccounts(): MutableList<Account>? {
--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/model/Account.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/data/model/Account.kt
@@ -3,5 +3,5 @@ package cl.jpinodev.walletarqlifecycle.feature_usuario.data.model
 data class Account (
     val cuenta_id: String = "",
     val user_id: String = "",
-    val balance: Double = 0.0
+    var balance: Double = 0.0
 )
