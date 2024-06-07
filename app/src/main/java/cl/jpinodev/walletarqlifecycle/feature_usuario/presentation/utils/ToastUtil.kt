d16cbf4b20f623e0885cdddc380658212823747e
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-06-07 12:02:44 -0400

    Implementa rediseño para Toasts

---
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/utils/ToastUtil.kt
@@ -0,0 +1,23 @@
+package cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.utils
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import cl.jpinodev.walletarqlifecycle.R
+
+object ToastUtils {
+    fun showCustomToast(context: Context, message: String) {
+        val inflater = LayoutInflater.from(context)
+        val layout: View = inflater.inflate(R.layout.custom_toast, null)
+
+        val text: TextView = layout.findViewById(R.id.toastText)
+        text.text = message
+
+        val toast = Toast(context)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.view = layout
+        toast.show()
+    }
+}
