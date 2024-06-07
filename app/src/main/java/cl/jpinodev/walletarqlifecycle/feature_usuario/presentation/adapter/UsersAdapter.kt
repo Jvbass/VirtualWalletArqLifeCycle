b4e1a9e1c9fc11c177268812e7f5c1ef3d6dc246
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-06-07 02:21:39 -0400

    Agrega imagen de usuario del recycler en el spinner de usuarios. Implementa funcionalidad par filtrar a usuario conectado y no mostrarlo en spinner

--- app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/adapter/UsersAdapter.kt
+++ app/src/main/java/cl/jpinodev/walletarqlifecycle/feature_usuario/presentation/adapter/UsersAdapter.kt
@@ -5,35 +5,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import cl.jpinodev.walletarqlifecycle.R
 import cl.jpinodev.walletarqlifecycle.feature_usuario.data.model.Usuario
+import cl.jpinodev.walletarqlifecycle.feature_usuario.presentation.viewmodel.WalletViewModel
 
 /*
 * Adaptador para mostrar una lista de usuarios en un Spinner.
 * Usamos BaseAdapter() para crear un adaptador base para que el Spinner sea soportado sin implementar
 * todos los métodos de la interfaz SpinnerAdapter.
  */
-class UsersAdapter(private val context: Context, private val usuarios: List<Usuario>) : BaseAdapter() {
+class UsersAdapter(private val context: Context, private val usuarios: List<Usuario>, private val viewModel: WalletViewModel) :
+    BaseAdapter() {
+
+    private val filteredUsuarios: List<Usuario> = usuarios.filter {
+        it.user_id != viewModel.usuarioConectado.value?.user_id
+    }
+
     override fun getCount(): Int {
-        return usuarios.size
+        return filteredUsuarios.size
     }
+
     override fun getItem(position: Int): Any {
-        return usuarios[position]
+        return filteredUsuarios[position]
     }
+
     override fun getItemId(position: Int): Long {
         return position.toLong()
     }
-       override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.users_item, parent, false)
-        val usuario = usuarios[position]
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view =
+            convertView ?: LayoutInflater.from(context).inflate(R.layout.users_item, parent, false)
+        val usuario = filteredUsuarios[position]
+        val imageResource = viewModel.getUserImageResource(usuario.user_id)
+
         val nombreUsuario = view.findViewById<TextView>(R.id.nombreUsuario)
         val apellidoUsuario = view.findViewById<TextView>(R.id.apellidoUsuario)
+        val imageUsuario = view.findViewById<ImageView>(R.id.imageUsuario)
+
         nombreUsuario.text = usuario.nombre
         apellidoUsuario.text = usuario.apellido
+        imageUsuario.setImageResource(imageResource)
+
         return view
     }
-      override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
         return getView(position, convertView, parent)
     }
-  }
+}
